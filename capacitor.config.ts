c01b556a3f465e0f4b147d2423e975502dcd1cfe
DawidWetzler <wetzlerdawid@gmail.com>
2022-04-19 18:15:56 +0200

    reproduction steps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionic-accordion-datetime-bug',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://127.0.0.1:8080',
+    cleartext: true,
+  },
 };
 
 export default config;
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,24 +5,54 @@
         <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>Bug when using ion-datetime within ion-accordion</strong>
+        <p>This occurs only on iOS devices</p>
+        <h6>Reproduction steps</h6>
+        <ul>
+          <ol>
+            <li>Open accordion</li>
+            <li>Click Next / Previous arrow</li>
+          </ol>
+        </ul>
+
+        <div>
+          <ion-accordion-group>
+            <ion-accordion>
+              <ion-item slot="header">
+                <ion-label>Open Datetime</ion-label>
+              </ion-item>
+
+              <ion-datetime slot="content" size="cover" />
+            </ion-accordion>
+          </ion-accordion-group>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonDatetime,
+  IonAccordion,
+  IonAccordionGroup,
+  IonLabel,
+  IonItem,
+} from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -32,37 +62,12 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonAccordionGroup,
+    IonAccordion,
+    IonDatetime,
+    IonLabel,
+    IonItem,
+  },
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
